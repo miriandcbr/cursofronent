4b9d51dae00596fc49a56ad3f759853169bb10e3
mirian bayas <mbayasr5@hotmail.com>
2021-11-26 07:56:07 -0500

    prueb

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello" v-if='true'>
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    
+    
+
+   <div>
+     <h1> titulo</h1>
+     <p>mi primer componente</p>
+    </div>
+
+
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return{
+        nombre:'Alex'
+      }
+    },
+methods :{
+      metodo1(){
+        }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
